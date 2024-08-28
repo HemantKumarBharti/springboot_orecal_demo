bb958f0937caf43b1f311a5523a6361e26682453
hemant bharti <hemantbharti1105@gmail.com>
2024-08-28 16:45:39 +0530

    project

---
+++ src/test/java/com/example/springboot_orecal_demo/SpringbootOrecalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_orecal_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOrecalDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
